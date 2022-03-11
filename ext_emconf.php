820e77c52f7da0d3d0b88ba75f342120f2264f6e
Francois Suter <>
2022-03-11 08:19:22 +0100

    Add ext_emconf file

---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Workspace preview bug test extension',
+    'description' => 'Workspace preview bug test extension',
+    'category' => 'plugin',
+    'author' => 'Francois Suter',
+    'author_email' => 'francois.suter@ideative.ch',
+    'author_company' => 'Ideative',
+    'state' => 'stable',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.0.0',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
